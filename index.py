fbd866901e419d7b20b6f47587b8af838697e644
Wasiur Rahman <osiur2@gmail.com>
2022-10-18 18:38:35 +0600

    Bismillah

---
+++ index.py
@@ -0,0 +1,20 @@
+amin = 100
+while(amin < 140):
+    print (1 + 2)
+    print(amin)
+    amin = amin + 1
+
+while(amin > 130):
+    print("Bari Jabo")
+    while(amin > 0):
+        print(100)
+        amin = amin - 50
+    print(amin)
+    amin = amin - 1
+
+
+
+
+
+
+
