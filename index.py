0fc0c9168f1af249bbcb0c603de26a6036adbd44
Wasiur Rahman <osiur2@gmail.com>
2022-10-18 19:30:24 +0600

    Add Arrey

--- index.py
+++ index.py
@@ -1,3 +1,8 @@
+from traceback import print_tb
+
+
+print ("Bismillahir Rahmanir Rahim")
+
 amin = 100
 while(amin < 140):
     print (1 + 2)
@@ -12,7 +17,13 @@ while(amin > 130):
     print(amin)
     amin = amin - 1
 
+student = ["Labid", "Asim", "Muhsin", "Antara", "Salim"]
+print(student[1-3])
 
+stuf = {"name:" "Amin", "age:" "25", "selary:" "50000"}
+{"name:" "Tuhin", "age:" "25", "selary:" "50000"}
+{"name:" "Saleh", "age:" "25", "selary:" "50000"}
+print(stuf)
 
 
 
