46a9ecd05c316ed1b7cd08bf5f2656dcc32c5c66
Wasiur Rahman <osiur2@gmail.com>
2022-10-22 10:14:51 +0600

    Loop

---
+++ loop.py
@@ -0,0 +1,58 @@
+student = "Labid", "Asim", "Muhsin", "Antara", "Salim"
+print(student[0:5])
+
+stuff = ["name: " "Amin", "age: " "25", "selary: " "40000",
+    "name: " "Tuhin", "age: " "35", "selary: " "60000",
+    "name: " "Saleh", "age: " "45", "selary: " "50000",]
+print(stuff)
+
+print(((300 + 200) - 250) * 12 / 2)
+
+lines = '''Allah is our Khaliq
+Muhammad is the Masenger
+Abdullah is his father
+Amena is his mother
+Baytullah is our Qibla'''
+print(lines)
+
+print(10 * ' Allah ')
+rab = ' Allah '
+print(rab * 10)
+
+name1 = 'Amin'
+name2 = 'Bashir'
+tahiya = '''Assalamu yalaikum, my brother %s and %s, How are you? 
+My students %s all are good'''
+students = 'Labib', 'Shahid', 'Salam', 'Nayem', 'Tuhin'
+print(tahiya % (name1, name2, students))
+
+list1 = 'a', 'b', 'c'
+list2 = 1, 2, 3 
+mylist = list1 + list2 
+print(mylist)
+
+age = '10'
+converted_age = int(age)
+age = 10
+converted_age = str(age)
+
+sl = 1
+for x in range(1, 6):
+    for y in range(101, 106):
+        print(sl)
+        print('Reg : %s' % x + ' Roll : %s' % y)
+        sl = sl + 1
+
+for i in range(1, 6):
+    print('Reg : %s ' % i)
+
+for s in students:
+    print(s)
+
+reg = 1
+roll = 101
+while(reg < 6 and roll < 116):
+    print (students, reg, roll)
+    reg = reg + 1
+    roll = roll + 1
+    
