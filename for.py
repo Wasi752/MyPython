71b4c96510e08eaced4ceb2e1bbfc5fe65a8564c
https://github.com/Wasi752 <osiur2@gmail.com>
2022-10-22 20:31:46 +0600

    Add Line

--- for.py
+++ for.py
@@ -8,5 +8,6 @@ for x in range(1, 6):
 for i in range(1, 6):
     print('Reg : %s ' % i)
 
+students = ['Labib', 'Shahid', 'Salam', 'Nayem', 'Tuhin','Labib', 'Shahid', 'Nayem', 'Labib', 'Shahid', 'Salam', 'Nayem']
 for s in students:
     print(s)
