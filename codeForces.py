10d9804e5b8b2dfc0033dda6b7f19bc8efa4eeb3
Wasiur Rahman <osiur2@gmail.com>
2022-11-01 15:18:41 +0600

    Update

---
+++ codeForces.py
@@ -0,0 +1,7 @@
+
+num = input()
+x = int(num) % 2
+if x == 0 and int(num) > 2:
+    print("YES")
+else: 
+    print("NO")
---
+++ problems.py
@@ -0,0 +1,14 @@
+'''
+print(1, 2, 3, 4, 5)
+x = 1
+while x < 501:
+    print(x)
+    x = x + 1
+
+num = input()
+reg = 0
+while reg <= int(num):
+    print(reg)
+    reg = reg + 1
+'''
+
