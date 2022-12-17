42f5ec18d255a8a213469a8b6f2fafe0e51abbcc
Wasiur Rahman <osiur2@gmail.com>
2022-12-17 13:41:40 +0600

    Beautiful Matrix

--- problems.py
+++ problems.py
@@ -1,10 +1,19 @@
-a = int(input())
-counter = 0
-while a > 0:
-    b = input()
-    if (b[1] == "+"):
-        counter = counter + 1
-    else:
-        counter = counter - 1
-    a = a - 1
-print(counter)
+r1 = list(map(int, input().split()))
+r2 = list(map(int, input().split()))
+r3 = list(map(int, input().split()))
+r4 = list(map(int, input().split()))
+r5 = list(map(int, input().split()))
+f = [r1, r2, r3, r4, r5]
+r = 0
+c   = 0 
+p = []
+while r < 5:
+    while c < 5:
+        if f[r][c] == 1:
+            p = [r, c]
+        c = c + 1
+    c = 0 
+    r = r + 1
+x = abs(2 - p[0])
+y = abs(2 - p[1])
+print(x + y)
