e79ed1679d53185438987635af7ff2b3910d9bd3
https://github.com/Wasi752 <osiur2@gmail.com>
2023-01-09 05:33:33 +0600

    Home Work

--- wasi.py
+++ wasi.py
@@ -10,3 +10,24 @@ while b <= a:
     sum = first + last
     print('Sum = %d' %sum)
     b = b + 1
+
+''' 1
+input:
+12345
+234
+17890
+output:
+15
+9
+25
+'''
+''' 2
+input:
+12345
+234
+17890
+output:
+even = 2, odd = 3
+even = 2, odd = 1
+even = 2, odd = 3
+'''
