b7f65c0e9c2e6b2e51aed2c1a99a03e5e8ea3d2b
https://github.com/Wasi752 <osiur2@gmail.com>
2023-01-09 05:25:28 +0600

    প্রোগ্রামিং সমস্যা ৬ — যোগফল নির্ণয়

--- wasi.py
+++ wasi.py
@@ -1,9 +1,12 @@
 a = int(input())
 b = 1
 while b <= a:
-    c = input()
-    first = c[0]
-    last = c[4]
-    sum = int(first) + int(last)
+    c = int(input()) # 56789 
+    last = c % 10
+    while c > 0:
+        first = c % 10
+        temp = c // 10
+        c = temp 
+    sum = first + last
     print('Sum = %d' %sum)
     b = b + 1
