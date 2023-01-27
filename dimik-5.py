f46437521dc2d312d81da70b53a13d78bb35c9ba
https://github.com/Wasi752 <osiur2@gmail.com>
2023-01-27 21:48:20 +0600

    Home Work-1 has done

---
+++ dimik-5.py
@@ -0,0 +1,27 @@
+'''
+t = int(input())
+i = 0
+while t > i:
+    x = int(input())
+    j = 0
+    while j < x:
+        k = 0
+        while k < x:
+            print("*", end='')
+            k = k + 1
+        print("")
+        j = j + 1
+    print('')
+    i = i + 1
+'''
+t = int(input())
+i = 0
+while t > i:
+    n = int(input())
+    j = 0
+    k = "*"
+    while j < n:
+        print(k)
+        j = j + 1
+        k = k + "*"
+    t = t - 1
