332d59a4d25983a9c6c96e1b4e73199bfe4f2397
https://github.com/Wasi752 <osiur2@gmail.com>
2023-01-28 21:58:36 +0600

    # ছোটো থেকে বড়ো

--- dimik-5.py
+++ dimik-5.py
@@ -1,18 +1,4 @@
-'''
-t = int(input())
-i = 0
-while t > i:
-    x = int(input())
-    j = 0
-    while j < x:
-        k = 0
-        while k < x:
-            print("*", end='')
-            k = k + 1
-        print("")
-        j = j + 1
-    print('')
-    i = i + 1
+
 
 t = int(input())
 i = 0
@@ -36,3 +22,4 @@ while t > i:
         print(k * n)
         n = n - 1
     t = t - 1
+    ''' 
