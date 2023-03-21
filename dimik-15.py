b7af3e5885ccdf4c435c835c6de2301431ec0e88
https://github.com/Wasi752 <osiur2@gmail.com>
2023-03-21 07:30:40 +0600

    dimik-15

--- dimik-15.py
+++ dimik-15.py
@@ -1,3 +1,14 @@
-# Home Work
-# charectar count
-# Vowel Count
+t = int(input())
+i = 0
+while i < t:
+    jumla = input()
+    ch = "abcdefghijklmnopqrstuvwxyz"
+    for x in ch:
+        counter = 0
+        for y in jumla:
+            if x == y:
+                counter = counter + 1
+        if counter != 0:
+            print("{} = {}".format(x, counter))
+    print('')
+    i = i + 1
